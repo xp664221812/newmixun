099e31d7fc6a6c9de81eb90c335fa9b68d0a17f2
xp664221812 <realxp141414@163.com>
2018-08-16 19:11:53 +0800

    first

---
+++ newmixun/src/main/java/cn/xp/newmixun/mvp/ui/activity/MainActivity.java
@@ -0,0 +1,25 @@
+package cn.xp.newmixun.mvp.ui.activity;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import com.jess.arms.base.BaseActivity;
+import com.jess.arms.di.component.AppComponent;
+
+public class MainActivity extends BaseActivity{
+    @Override
+    public void setupActivityComponent(@NonNull AppComponent appComponent) {
+
+    }
+
+    @Override
+    public int initView(@Nullable Bundle savedInstanceState) {
+        return 0;
+    }
+
+    @Override
+    public void initData(@Nullable Bundle savedInstanceState) {
+
+    }
+}
